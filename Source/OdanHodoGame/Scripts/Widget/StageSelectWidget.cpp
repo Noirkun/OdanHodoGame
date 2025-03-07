be08364717fa20f2b2a04e57f4688f913761bc77
Noirkun <noirkun2306@gmail.com>
2025-03-08 00:55:25 +0900

    StageSelectWidget.hとcppの修正、セレクト画面用の木の背景画像の追加

--- Source/OdanHodoGame/Scripts/Widget/StageSelectWidget.cpp
+++ Source/OdanHodoGame/Scripts/Widget/StageSelectWidget.cpp
@@ -2,7 +2,7 @@
 
 
 #include "StageSelectWidget.h"
-
+#include "Components/TextBlock.h"
 
 void UStageSelectWidget::NativeConstruct()
 {
@@ -11,15 +11,26 @@ void UStageSelectWidget::NativeConstruct()
 	for (auto StageData : StageDataAsset->StageData)
 	{
 		LoopStageData(StageData);
+		StageDataNum++;
+		UE_LOG(LogTemp,Warning,TEXT("StageDataNum:%d"),StageDataNum);
 	}
+	//選ばれているステージの名前をステージデータの名前にする
+	ForceStageName=StageDataAsset->StageData[ForceIndex].StageName;
+	//選ばれているステージのレベルをステージデータのレベルにする
+	ForceStageLevel=StageDataAsset->StageData[ForceIndex].StageLevel;
+
+	//ステージの名前を表示するWidgetにステージの名前を入れる
+	TitleText->SetText(ForceStageName);
+	
 }
 
+//BPでLoopstageを出す関数
 void UStageSelectWidget::LoopStageData(FStageStruct StageData)
 {
-	//BP用の関数を呼び出す
 	BP_LoopStageData(StageData);
 }
 
+//HorizontalBoxのポジションを変える関数
 void UStageSelectWidget::ScrollStageList(float InXPos, float InYPos, float StageWidget, float PosXOffset, float& PosX,
 	float& PosY)
 {
@@ -29,6 +40,61 @@ void UStageSelectWidget::ScrollStageList(float InXPos, float InYPos, float Stage
 	float TempDestination=ForceIndex*StageWidget + PosXOffset;
 	//X軸の移動の計算結果をいれる
 	PosX=InXPos+(TempDestination-=InXPos)*0.1f;
+}
+
+//次のリストウィジェットに移動する関数
+void UStageSelectWidget::NextListWidget()
+{
+	UE_LOG(LogTemp,Warning,TEXT("ForceIndex:%d"),ForceIndex);
+	UE_LOG(LogTemp,Warning,TEXT("StageDataNum:%d"),StageDataNum);
+	//選ばれているステージのIndexを増やす
+	ForceIndex++;
+	
+	//選ばれているステージのIndexがステージデータの数より大きい場合
+	if(ForceIndex>=StageDataNum)
+	{
+		//選ばれているステージのIndexを0に戻す
+		ForceIndex = 0;
+		//選ばれているステージの名前をステージデータの名前にする
+		ForceStageName=StageDataAsset->StageData[ForceIndex].StageName;
+		//選ばれているステージのレベルをステージデータのレベルにする
+		ForceStageLevel=StageDataAsset->StageData[ForceIndex].StageLevel;
+	}
+	else
+	{
+		//選ばれているステージの名前をステージデータの名前にする
+		ForceStageName=StageDataAsset->StageData[ForceIndex].StageName;
+		//選ばれているステージのレベルをステージデータのレベルにする
+		ForceStageLevel=StageDataAsset->StageData[ForceIndex].StageLevel;
+	}
+}
+
+//前のリストウィジェットに移動する関数
+void UStageSelectWidget::PrevListWidget()
+{
+	UE_LOG(LogTemp,Warning,TEXT("ForceIndex:%d"),ForceIndex);
+	UE_LOG(LogTemp,Warning,TEXT("StageDataNum:%d"),StageDataNum);
+	//選ばれているステージのIndexを減らす
+	ForceIndex--;
+
+	UE_LOG(LogTemp,Warning,TEXT("ForceIndex:%d"),ForceIndex);
+	//選ばれているステージのIndexが0より小さい場合
+	if(ForceIndex<0)
+	{
+		//選ばれているステージのIndexをステージデータの数-1にする
+		ForceIndex = StageDataNum-1;
+		//選ばれているステージの名前をステージデータの名前にする
+		ForceStageName=StageDataAsset->StageData[ForceIndex].StageName;
+		//選ばれているステージのレベルをステージデータのレベルにする
+		ForceStageLevel=StageDataAsset->StageData[ForceIndex].StageLevel;
+	}
+	else
+	{
+		//選ばれているステージの名前をステージデータの名前にする
+		ForceStageName=StageDataAsset->StageData[ForceIndex].StageName;
+		//選ばれているステージのレベルをステージデータのレベルにする
+		ForceStageLevel=StageDataAsset->StageData[ForceIndex].StageLevel;
+	}
 }
 
 
--- Source/OdanHodoGame/Scripts/Widget/StageSelectWidget.h
+++ Source/OdanHodoGame/Scripts/Widget/StageSelectWidget.h
@@ -7,6 +7,7 @@
 #include "OdanHodoGame/Scripts/DataAssets/StageDataAsset.h"
 #include "StageSelectWidget.generated.h"
 
+class UTextBlock;
 /**
  * ステージ選択用のWidgetクラス
  */
@@ -24,12 +25,28 @@ public:
 	void BP_LoopStageData(FStageStruct StageData);
 
 	//選ばれているステージのIndex
-	UPROPERTY(VisibleAnywhere,BlueprintReadWrite, Category = "Data Setting")
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly, Category = "Data Setting")
 	int32 ForceIndex = 0;
+
+	//選ばれているステージの名前
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly, Category = "Data Setting")
+	FText ForceStageName;
+
+	//選ばれているステージのレベル
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly, Category = "Data Setting")
+	TSoftObjectPtr<UWorld> ForceStageLevel;
+
+	//ステージの名前を表示するWidget
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,meta = (BindWidget))
+	UTextBlock* TitleText = nullptr;
+	
 private:
 	//ステージデータアセット
 	UPROPERTY(EditAnywhere, Category = "Data Setting")
 	UStageDataAsset* StageDataAsset = nullptr;
+
+	//ステージデータの数
+	int32 StageDataNum = 0;
 	
 	//StageDataAssetのデータ分ループする関数
 	UFUNCTION()
@@ -44,7 +61,15 @@ private:
 	//! @param PosY Y座標の戻り値
 	UFUNCTION(BlueprintCallable, Category = "StageSelect")
 	void ScrollStageList(float InXPos,float InYPos,float StageWidget, float PosXOffset, float& PosX, float& PosY);
-	
+
+	//次のリストウィジェットに移動する関数
+	UFUNCTION(BlueprintCallable, Category = "StageSelect")
+	void NextListWidget();
+
+	//前のリストウィジェットに移動する関数
+	UFUNCTION(BlueprintCallable, Category = "StageSelect")
+	void PrevListWidget();
+
 	
 	
 };
